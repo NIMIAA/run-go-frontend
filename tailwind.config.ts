24aa1e47f6f710e70c2470e7f64cb589abf002ab
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-01-22 20:03:25 +0100

    chore: add app color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import colors from "tailwindcss/colors";
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,10 +7,46 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        ...colors,
+        app_blue: {
+          DEFAULT: "#000066",
+          50: "#1F1FFF",
+          100: "#0A0AFF",
+          200: "#0000E0",
+          300: "#0000B8",
+          400: "#00008F",
+          500: "#000066",
+          600: "#00002E",
+          700: "#000000",
+          800: "#000000",
+          900: "#000000",
+          950: "#000000",
+        },
+        app_yellow: {
+          DEFAULT: "#CC9933",
+          50: "#F1E2C6",
+          100: "#EDDAB6",
+          200: "#E4CA95",
+          300: "#DCBA74",
+          400: "#D4A954",
+          500: "#CC9933",
+          600: "#9F7728",
+          700: "#72561D",
+          800: "#453411",
+          900: "#181206",
+          950: "#020201",
+        },
       },
     },
   },
