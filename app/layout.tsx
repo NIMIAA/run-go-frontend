56c772b1b1d552f1da986c1e255fa4385bbea7e7
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-03-22 22:58:42 +0100

    refactor: enforce base font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
 
-import { Geist, Geist_Mono} from "next/font/google";
-
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-
+const poppins = Poppins({ weight: "400" });
 
 export const metadata: Metadata = {
   title: "RUN GO",
@@ -28,11 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        // className={`  antialiased`}
-      >
-        {children}
-      </body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
