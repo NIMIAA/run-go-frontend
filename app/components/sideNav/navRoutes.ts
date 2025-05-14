1e6d88c69627f6d1b9ed6da02a9e860cf8040f50
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-05-14 18:03:51 +0100

    make dashboard responsive

--- app/components/sideNav/navRoutes.ts
+++ app/components/sideNav/navRoutes.ts
@@ -1,34 +1,48 @@
-import { Squares2X2Icon,TruckIcon, CreditCardIcon, ChatBubbleBottomCenterTextIcon, Cog6ToothIcon, ArrowRightStartOnRectangleIcon } from "@heroicons/react/24/outline";
+import {
+  Squares2X2Icon,
+  TruckIcon,
+  CreditCardIcon,
+  ChatBubbleBottomCenterTextIcon,
+  Cog6ToothIcon,
+} from "@heroicons/react/24/outline";
 
-export const navRoutes = [
-    {
-        label: "Dashboard",
-        path: "/user_dashboard/dashboard",
-        icon: Squares2X2Icon,
-    },
-    {
-        label: "Rides",
-        path: "/user_dashboard/dashboard/rides",
-        icon: TruckIcon,
-    },
-    {
-        label: "Payments",
-        path: "/user_dashboard/dashboard/payments",
-        icon: CreditCardIcon,
-    },
-    {
-        label: "Messages",
-        path: "/user_dashboard/dashboard/messages",
-        icon: ChatBubbleBottomCenterTextIcon,
-    },
-    {
-        label: "Settings",
-        path: "/user_dashboard/dashboard/settings",
-        icon: Cog6ToothIcon,
-    },
-    {
-        label: "Logout",
-        path: "/user_dashboard/logout",
-        icon: ArrowRightStartOnRectangleIcon,
-    },
+import {
+  Squares2X2Icon as SolidSquares2X2Icon,
+  TruckIcon as SolidTruckIcon,
+  CreditCardIcon as SolidCreditCardIcon,
+  ChatBubbleBottomCenterTextIcon as SolidChatBubbleBottomCenterTextIcon,
+  Cog6ToothIcon as SolidCog6ToothIcon,
+} from "@heroicons/react/24/solid";
+
+export const routes = [
+  {
+    label: "Dashboard",
+    path: "/user_dashboard/dashboard",
+    icon: Squares2X2Icon,
+    activeIcon: SolidSquares2X2Icon,
+  },
+  {
+    label: "Rides",
+    path: "/user_dashboard/dashboard/rides",
+    icon: TruckIcon,
+    activeIcon: SolidTruckIcon,
+  },
+  {
+    label: "Payments",
+    path: "/user_dashboard/dashboard/payments",
+    icon: CreditCardIcon,
+    activeIcon: SolidCreditCardIcon,
+  },
+  {
+    label: "Messages",
+    path: "/user_dashboard/dashboard/messages",
+    icon: ChatBubbleBottomCenterTextIcon,
+    activeIcon: SolidChatBubbleBottomCenterTextIcon,
+  },
+  {
+    label: "Settings",
+    path: "/user_dashboard/dashboard/settings",
+    icon: Cog6ToothIcon,
+    activeIcon: SolidCog6ToothIcon,
+  },
 ];
--- app/user_dashboard/dashboard/layout.tsx
+++ app/user_dashboard/dashboard/layout.tsx
@@ -1,18 +1,82 @@
-import React from 'react';
-import SideNav from '../../components/sideNav'; 
+"use client";
+
+import React from "react";
+import { BellIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/16/solid";
+import Link from "next/link";
+import { routes } from "@/app/components/sideNav/navRoutes";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+
 export default function DashboardViewLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
   return (
-    <div>
-      <div className="flex flex-row h-screen bg-gray-100">
-        <div className="w-1/5 bg-white shadow-md">
-          <SideNav />
-        </div>
-        <div className="w-4/5 p-4">{children}</div>
+    <>
+      <div className="md:grid grid-cols-12 h-screen w-full">
+        <nav className="  col-span-2 pt-12 relative bg-foreground/95 text-white hidden md:block">
+          <ul className="flex flex-col gap-y-6 px-4">
+            {routes.map((route) => {
+              return (
+                <li key={route.label} className="">
+                  <Link
+                    className={
+                      route.path == pathname
+                        ? "flex font-[family-name:var(--font-geist-sans)] items-center gap-x-2 prose-sm capitalize font-medium text-center py-2 px-2 transition delay-25 ease-in-out duration-200 bg-white/90 rounded-lg text-foreground"
+                        : "flex font-[family-name:var(--font-geist-sans)] items-center gap-x-2 prose-sm capitalize font-medium text-center py-2 px-2 transition delay-25 ease-in-out duration-200 hover:bg-white/90 rounded-lg hover:text-foreground"
+                    }
+                    href={route.path}
+                  >
+                    {" "}
+                    <route.activeIcon className="h-6 w-6" aria-hidden="true" />
+                    {route.label}
+                  </Link>
+                </li>
+              );
+            })}
+
+            <button className="position absolute bottom-10 left-4 right-4   flex font-[family-name:var(--font-geist-sans)] items-center gap-x-2 prose-sm capitalize font-medium text-center py-2 px-2 transition delay-25 ease-in-out duration-200 hover:bg-white/90 rounded-lg hover:text-foreground">
+              <ArrowLeftStartOnRectangleIcon className="size-6" /> Logout
+            </button>
+          </ul>
+        </nav>
+
+        <main className="col-span-10 h-screen overflow-x-hidden overflow-y-scroll">
+          <section
+            role="header"
+            className="flex justify-between align-center border-b-gray-200 border-b-[2px] max-h-24 bg-white py-4 px-4 md:px-16"
+          >
+            <Bars3Icon className="size-8 block sm:hidden" />
+            <label className="input rounded-lg w-3/5 hover:border-foreground hover:outline-transparent focus:outline-none hidden md:block">
+              <input type="search" required placeholder="Search" className="" />
+            </label>
+            <div className="flex items-center gap-x-2 md:gap-x-6">
+              <div className="cursor-pointer">
+                <BellIcon className="size-6 hidden md:block" />
+              </div>
+              <div className="cursor-pointer flex items-center gap-x-2">
+                <div className="avatar ">
+                  <div className="size-8  md:size-12 rounded-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                  </div>
+                  <div></div>
+                </div>
+                <div className="hidden md:block">
+                  <h6 className="-mb-2 font-medium">Jane Doe</h6>
+                  <small>jane@rungo.com</small>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="py-12 px-16 h-[90vh] overflow-y-scroll bg-white/70">
+            {children}
+          </section>
+        </main>
       </div>
-    </div>
+    </>
   );
-};
+}
